b5ae44fad08ccc86d7fa572d2773453a3d40e5c6
alfonsojim1897 <alfonsojim1897@gmail.com>
2024-10-13 19:20:35 +0200

    Refactor user-profile.js to include session provider for user authentication

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,8 +1,18 @@
+import { useSession } from 'next-auth/react';
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
 
 function UserProfile() {
+  const { data: session, status } = useSession();
   // Redirect away if NOT auth
+
+  if (status === 'loading') {
+    return <p className={classes.profile}>Loading...</p>;
+  }
+
+  if (status === 'unauthenticated') {
+    window.location.href = '/auth';
+  }
 
   return (
     <section className={classes.profile}>
