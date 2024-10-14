01e091a29c140bfd76207f3508b1e27d41ad1e61
alfonsojim1897 <alfonsojim1897@gmail.com>
2024-10-14 11:45:35 +0200

    Refactor auth.js to include session provider for user authentication

--- pages/auth.js
+++ pages/auth.js
@@ -1,6 +1,17 @@
 import AuthForm from '../components/auth/auth-form';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 function AuthPage() {
+  const router = useRouter();
+  const { data: session, status } = useSession();
+  if (status === 'authenticated') {
+    router.replace('/');
+  }
+
+  if (status === 'loading') {
+    return <p className="center">Loading...</p>;
+  }
   return <AuthForm />;
 }
 
