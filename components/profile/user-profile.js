bbc91d1232f43c9b5a87647ec1ecb4dc25debacb
alfonsojim1897 <alfonsojim1897@gmail.com>
2024-10-14 12:41:53 +0200

    Refactor user-profile.js to include changePasswordHandler in UserProfile component

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -2,10 +2,28 @@ import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
 
 function UserProfile() {
+  const changePasswordHandler = async (passwordData) => {
+    const response = await fetch('/api/user/change-password', {
+      method: 'PATCH',
+      body: JSON.stringify(passwordData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      console.log(data.message || 'Something went wrong!');
+    } else {
+      console.log(data.message || 'Password updated successfully!');
+    }
+  };
+
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm onChangePassword={changePasswordHandler} />
     </section>
   );
 }
